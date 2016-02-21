7654183950e1d60ef705b20ca0854950cad71197
Jaewon Kim <jaewonism57@gmail.com>
2016-02-21 17:04:01 +0900

    commit for configuration

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.send(process.env.MESSAGE || 'Default message!');
 });
 
 var port = Number(process.env.PORT || 5000);
